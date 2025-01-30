dce6b136b7bbc12e74fbf68b9a90cce415fcb411
Ankit  Mishra <mishraankit5252ankit@gmail.com>
2025-01-30 23:14:24 +0530

    new

--- src/Hash_Map.java
+++ src/Hash_Map.java
@@ -4,5 +4,24 @@ import java.util.Map;
 public class Hash_Map {
     public static void main(String[] args) {
         Map<Integer, String> c = new HashMap<>();
+        c.put(10,"Ankit");
+        c.put(11,"Aman");
+        c.put(12,"Aditya");
+        c.put(13,"Alok");
+        System.out.println(c); // ------> storing the value in hashmap the form of the key and value.
+
+        c.putIfAbsent(10,"DCCn");
+        System.out.println(c); // ------> checking the given key is present or not in this hashmap, if the key is present in the hashmap they print the presented value.
+
+        System.out.println(c.isEmpty()); // ----------> They checks the hash map contain any value or not.
+
+        c.remove(10);
+        System.out.println(c); // ---------> It removes the key value of the 10.
+
+        for (int d:c.keySet()){
+            System.out.println(d); // ------> They prints the all the keys.
+        } for (String f:c.values()){
+            System.out.println(f); // -------> They print the all the values.
+        }
     }
 }
---
+++ src/Tree_Map.java
@@ -0,0 +1,9 @@
+import java.util.TreeMap;
+import java.util.Map;
+
+public class Tree_Map {
+    public static void main(String[] args) {
+        Map<Integer, String> c = new TreeMap<>();
+        c.put()
+    }
+}
