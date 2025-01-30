0dc754006a8dd11c8b42df9c2dc8b07c1a5ae255
Ankit  Mishra <mishraankit5252ankit@gmail.com>
2025-01-30 21:49:27 +0530

    Create Hash_Map.java

---
+++ src/Hash_Map.java
@@ -0,0 +1,8 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class Hash_Map {
+    public static void main(String[] args) {
+        Map<Integer, String> c = new HashMap<>();
+    }
+}
